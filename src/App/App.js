254cf79ec3c7fc4f5693673195faa36e748769fd
BesoinR <remi.fujitsu@fujitsu.com>
2021-09-21 16:21:55 +0200

    MemeEditor fonctionnel

--- src/App/App.js
+++ src/App/App.js
@@ -3,19 +3,21 @@ import styles from './App.module.scss'
 import ThumbnailLayout from "./components/layout/ThumbnailLayout/ThumbnailLayout";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
 import {REST_ADR, RESOURCES} from "./config/config";
+import FlexLayout from "./components/layout/FlexLayout/FlexLayout";
+import MemeEditor from "./components/MemeEditor/MemeEditor";
 
 const initialState = {
     memes: [],
     current:
         {
-            imageId: 0,
+            imageId: 1,
             x: 100,
             y: 300,
             text: "Mon premier meme",
             name: "Meme name",
             style: {
                 fontSize: 60,
-                fill: "white",
+                fill: "#ACACAC",
                 textDecoration: "underline",
                 fontStyle: "normal",
                 fontWeight: 500
@@ -52,8 +54,25 @@ class App extends React.Component{
 
     render() {
         return (
-                <div className={styles.App}>
-                </div>
+            <div className={styles.App}>
+                <div>{JSON.stringify(this.state.current)}</div>
+                <FlexLayout>
+                    <MemeViewer meme=
+                                    {{
+                                        ...this.state.current,
+                                        image: this.state.images
+                                            .find(e => e.id === this.state.current.imageId)
+                                    }}
+                    />
+                    <MemeEditor
+                        meme={this.state.current}
+                        images={this.state.images}
+                        onFormChange={(currentInForm) => {
+                            this.setState({current: currentInForm});
+                        }}
+                    />
+                </FlexLayout>
+            </div>
         )
     }
 }
--- src/App/components/MemeViewer/MemeViewer.tsx
+++ src/App/components/MemeViewer/MemeViewer.tsx
@@ -25,19 +25,30 @@ interface MemeViewerProps {
 }
 
 const MemeViewer = (props) => {
+
+    const img = props.meme.image ? props.meme.image : {w: 1000, h: 1000, url: ''};
+
     return (
-        <svg className={styles.MemeViewer}
-             data-testid="MemeViewer"
-             viewBox={`0 0 ${props.meme.image.w} ${props.meme.image.h}`}>
-            <image href={props.meme.image.url}/>
-            <text
-                x={props.meme.x}
-                y={props.meme.y}
-                style={props.meme.style}
-            >
-                {props.meme.text}
-            </text>
-        </svg>
+        <>
+            <svg className={styles.MemeViewer}
+                 data-testid="MemeViewer"
+                 viewBox={`0 0 ${img.w} ${img.h}`}>
+                <image href={img.url}/>
+                <text
+                    x={20}
+                    y={20}
+                >
+                    {props.meme.name}
+                </text>
+                <text
+                    x={props.meme.x}
+                    y={props.meme.y}
+                    style={props.meme.style}
+                >
+                    {props.meme.text}
+                </text>
+            </svg>
+        </>
     );
 }
 
